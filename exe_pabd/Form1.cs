31da4d2e0309e0a8b8abcb11b151c23bae2295c3
ilhamardypratama10jogja <ilhamardy10@gmail.com>
2022-06-16 14:49:43 +0700

    Add project files.

---
+++ exe_pabd/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace exe_pabd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection koneksi = new SqlConnection("Data Source=ILHAMARDY2001;Initial Catalog=exepabd;User ID=sa;Password=1234");
+            SqlDataAdapter sda = new SqlDataAdapter("select count(*) from login where NamaUser = '" + textBox1.Text + "'  and Password = '" +textBox2.Text + "'",koneksi);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Home panggil = new Home();
+                panggil.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("mohon isis user name dan password dengan benar !", "perhatian", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+    }
+}
---
+++ exe_pabd/Home.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace exe_pabd
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ILHAMARDY2001;Initial Catalog=exepabd;User ID=sa;Password=1234");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("insert into UserTable values (@ID,@Name,@Age)",con);
+            cmd.Parameters.AddWithValue("@ID",int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Succesfully saved");
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ILHAMARDY2001;Initial Catalog=exepabd;User ID=sa;Password=1234");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Update  UserTable set Name=@Name, Age=@Age where ID =@ID", con);
+
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Succesfully Updated");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ILHAMARDY2001;Initial Catalog=exepabd;User ID=sa;Password=1234");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Delete  UserTable where ID=@ID", con);
+
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            MessageBox.Show("Succesfully Deleted");
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=ILHAMARDY2001;Initial Catalog=exepabd;User ID=sa;Password=1234");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("Select  * from UserTable where ID=@ID", con);
+            cmd.Parameters.AddWithValue("ID", int.Parse(textBox1.Text));
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+           
+        }
+    }
+}
